cf115548c16ccdbdf062563b86873d35839e7883
Håkon Krogh <hakonhk@gmail.com>
2020-09-23 11:54:41 +0200

    chore: set node version
    BREAKING CHANGE

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var currentNodeVersion = process.versions.node;
+var semver = currentNodeVersion.split('.');
+var major = semver[0];
+
+if (major < 10) {
+  console.error(
+    'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'Crystallize CLI requires Node 10 or higher. \n' +
+      'Please update your version of Node.'
+  );
+  process.exit(1);
+}
+
 require('./src/cli.js');
