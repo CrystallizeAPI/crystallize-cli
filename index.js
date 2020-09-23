9f31ed6520cf5499681b109125754d7835d4efbc
Håkon Krogh <hakonhk@gmail.com>
2020-09-23 11:57:27 +0200

    fix: require node 8
    BREAKING CHANGE

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var currentNodeVersion = process.versions.node;
 var semver = currentNodeVersion.split('.');
 var major = semver[0];
 
-if (major < 10) {
+if (major < 8) {
   console.error(
     'You are running Node ' +
       currentNodeVersion +
       '.\n' +
-      'Crystallize CLI requires Node 10 or higher. \n' +
+      'Crystallize CLI requires Node 8 or higher. \n' +
       'Please update your version of Node.'
   );
   process.exit(1);
