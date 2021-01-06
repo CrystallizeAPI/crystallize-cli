1869613892680c4f4cbb61333749e01ecd3f3931
Håkon Krogh <hakonhk@gmail.com>
2021-01-06 20:10:21 +0100

    fix: proper check for node version

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var currentNodeVersion = process.versions.node;
 var semver = currentNodeVersion.split('.');
 var major = semver[0];
 
-if (major < 8) {
-  console.error(
-    'You are running Node.js v' +
-      currentNodeVersion +
-      '.\n' +
-      'Crystallize CLI requires Node 8 or higher. \n' +
-      'Please update your version of Node.'
-  );
+const minVersion = 12;
+
+if (major < minVersion) {
+  console.error(`
+You are running Node.js v${currentNodeVersion}
+Crystallize CLI requires Node ${minVersion} or higher.
+Please update your version of Node.
+`);
   process.exit(1);
 }
 
