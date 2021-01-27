b30c1ef629c7f43e0822e4dc3b4a08c97a618b31
Håkon Krogh <hakonhk@gmail.com>
2021-01-27 14:48:03 +0100

    fix: nextjs init no longer has payment setup

--- src/cli-utils/init-nextjs.js
+++ src/cli-utils/init-nextjs.js
@@ -20,9 +20,6 @@ async function initNextJS({ answers, projectPath }) {
 		locales: [],
 	};
 
-	// Set payment methods
-	appConfig.paymentProviders = answers.paymentMethods;
-
 	// Set locales
 	const locales = answers.multilingual || ['en'];
 	locales.forEach(function addToConfig(lng) {
@@ -53,33 +50,6 @@ async function initNextJS({ answers, projectPath }) {
 		'utf-8'
 	);
 
-	// Include Stripe credentials if stripe is selected
-	if (answers.paymentMethods.includes('stripe')) {
-		envLocalVars.STRIPE_SECRET_KEY = '';
-		envLocalVars.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY = '';
-	}
-
-	// Include Klarna credentials if klarna is selected
-	if (answers.paymentMethods.includes('klarna')) {
-		envLocalVars.KLARNA_USERNAME = '';
-		envLocalVars.KLARNA_PASSWORD = '';
-	}
-
-	// Include Vipps credentials if selected
-	if (answers.paymentMethods.includes('vipps')) {
-		envLocalVars.VIPPS_CLIENT_ID = '';
-		envLocalVars.VIPPS_CLIENT_SECRET = '';
-		envLocalVars.VIPPS_MERCHANT_SERIAL = '';
-		envLocalVars.VIPPS_SUB_KEY = '';
-	}
-
-	// Include Mollie credentials if selected
-	if (answers.paymentMethods.includes('mollie')) {
-		envLocalVars.MOLLIE_API_KEY = '';
-	}
-
-	envLocalVars.SENDGRID_API_KEY = '';
-
 	// Update app.config.json file
 	fs.writeFileSync(
 		path.resolve(projectPath, 'app.config.json'),
@@ -101,12 +71,6 @@ async function initNextJS({ answers, projectPath }) {
 			.map((key) => `${key}=${envLocalVars[key]}`)
 			.join(os.EOL) + os.EOL
 	);
-
-	// Cleanup payment providers
-	require(path.resolve(
-		projectPath,
-		'_repo-utils/cleanup-payment-providers.js'
-	));
 
 	// Add a sensible .gitignore
 	fs.writeFileSync(
